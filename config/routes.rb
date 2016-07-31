c96e02ad379cd1aba4491fa270196e5cb1b325d1
azurahi <azrx83@gmail.com>
2016-07-31 16:21:21 +0000

    map

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'map#index'
+
   devise_for :users
-  root 'home#index'
+  # root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
